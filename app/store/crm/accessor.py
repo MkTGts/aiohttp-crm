ec8a6ea57d7fdb10b2a1dca83608600306d8a7f3
mktgts <mktgts@debian-desktop.home>
2025-03-19 23:07:57 +0300

    Add accessor. Error

--- app/store/crm/accessor.py
+++ app/store/crm/accessor.py
@@ -1,11 +1,28 @@
 from app.crm.models import User
-from typing import Optional
-from app.web.app import Application
+from typing import Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from app.web.app import Application
 
 
 class CrmAccessor:
     def __init__(self):
         self.app: Optional[Application] = None
 
-    def add_user(self, user: User):
-        self.app.database['users'].append(users)
+
+    async def connect(self, app: "Application"):
+        self.app = app
+        try:
+            self.app.database["users"]
+        except KeyError:
+            self.app.database["users"] = []
+        print("connect to database")
+
+
+    async def disconnect(self, app: "Application"):
+        self.app = None
+        print("disconnect from database")
+
+
+    async def add_user(self, user: User):
+        self.app.database['users'].append(user)
--- app/web/app.py
+++ app/web/app.py
@@ -1,8 +1,8 @@
 from aiohttp.web import Application as AiohttpAplication, run_app as aiohttp_run_app, View as AiohttpView, Request as AiohttpRequset
 from app.crm.routes import setup_routes
-from typing import Optional
+from typing import Optional, TYPE_CHECKING
 from app.store.crm.accessor import CrmAccessor
-
+from app.store import setup_accessor
 
 
 class Application(AiohttpAplication):
@@ -27,4 +27,5 @@ app = AiohttpAplication()
 
 def run_app():
     setup_routes(app)
+    setup_accessor(app)
     aiohttp_run_app(app)
